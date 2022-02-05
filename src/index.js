9645760693007ff3e8f0c5f749e4865fdbeb114d
LisaDobruk <liskam8525gmail.com>
2022-02-05 16:17:44 +0300

    cool

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    for(let i = 0; i < expr.length / 10; i++){
+        arr.push(expr.substring(i * 10, (i + 1) * 10));
+    }
+    let arr2 = [];
+    arr.forEach((elem)=>{
+       let arrEl = '';
+       if(elem == '**********'){
+        arrEl = arrEl + ' ';
+        } 
+        for(let m = 0; m < elem.length / 2; m++){
+            
+            let arrElLit = elem.substring(m * 2, (m + 1) * 2);
+            if(arrElLit == '00') continue;
+            if(arrElLit == '10'){
+                arrEl = arrEl + '.';
+            }else if(arrElLit == '11'){
+                arrEl = arrEl + '-';
+            }
+        }
+        arr2.push(arrEl);
+    })
+    let str = ''
+    arr2.forEach((elem)=>{
+        if(elem == ' '){
+            str = str + ' ';
+        }else{
+         let strPart = MORSE_TABLE[elem];
+         str = str + strPart;
+        }
+    })
+   return str;
 }
 
 module.exports = {
